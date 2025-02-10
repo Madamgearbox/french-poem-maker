92c6c2cbc6be11bed6d56f5538dce267b843a985
Madamgearbox <madamgearbox@gmail.com>
2025-02-09 21:58:00 -0500

    changed title

--- src/script.js
+++ src/script.js
@@ -1,5 +1,4 @@
 function displayPoem(response) {
-    console.log("poem generated");
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
@@ -11,22 +10,18 @@ function displayPoem(response) {
 function generatePoem(event) {
   event.preventDefault();
 
-    let instructionsInput = document.querySelector("user-instructions");
-  let apiKey = "012b5b3a44t6b0bodaf08aa43444f3f6";
-  let prompt = `User instructions: Generate a French poem about $(instructionsInput.value)`;
-  let context = "You are a romantic Poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic terms and separate each line with a <br />. Make sure to follow the user instructions. Does not include a title to the poem. Please sign the poem with 'SheCodes AI' inside a <strong></strong> element";
-  let apiURL =
-    `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-  
-let poemElement = document.querySelector("#poem");
-poemElement.classList.remove("hidden");
-poemElement.innerHTML = `<div class="generating">Generating a French poem about ${instructionsInput.value}`;
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "2046c535afeb092fo82f1d306d8a2b2t";
+  let context =
+    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+  let prompt = `User instructions: Generate a French poem about ${instructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-console.log("Generating poem");
-console.log('Prompt: ${prompt}');
-console.log('Context: ${context}');
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${instructionsInput.value}</div>`;
 
-    axios.get(apiURL).then(displayPoem);
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
